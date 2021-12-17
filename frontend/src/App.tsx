d9e0593bc0841fbe82e10c6523116426c76b4545
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-12-17 15:47:45 -0500

    successfully built authurl

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,53 @@
-import React from "react";
 import "./App.css";
 
-// require("dotenv").config();
-
 function App() {
-	console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
+	function generateRandomString(length: number): string {
+		let text: string = "";
+		const possible: string =
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		for (let i = 0; i < length; i++) {
+			text =
+				text +
+				possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+
+		return text;
+	}
+
+	const state = generateRandomString(16);
+	localStorage.setItem("stateKey", state);
+	const scopes: string = "playlist-read-collaborative playlist-read-private";
+
+	function buildSpotifyAuthRequestURL() {
+		const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+		const redirect_url = process.env.REACT_APP_SPOTIFY_REDIRECT_URL;
+		const authUrl = "https://accounts.spotify.com/authorize";
+
+		if (
+			typeof clientSecret === "string" &&
+			typeof redirect_url === "string" &&
+			state.length > 15
+		) {
+			return (
+				authUrl +
+				"?response_type=token" +
+				`&client_id=${encodeURIComponent(clientSecret)}` +
+				`&scope=${scopes}` +
+				`&redirect_uri=${encodeURIComponent(redirect_url)}` +
+				`&state=${encodeURIComponent(state)}`
+			);
+		} else {
+			console.log("error");
+		}
+	}
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				Convert Your Spotify Playlist to Apple Music
 			</header>
-			<button onClick={() => console.log("Click")}>
+			<button onClick={() => console.log(buildSpotifyAuthRequestURL())}>
 				Log Into Spotify
 			</button>
 		</div>
