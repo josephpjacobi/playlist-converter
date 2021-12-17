e4eb1cb31b78884ee33a01a263043b26925d13f2
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-12-17 14:36:29 -0500

    implemented env vars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 
+// require("dotenv").config();
+
 function App() {
+	console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				Convert Your Spotify Playlist to Apple Music
 			</header>
+			<button onClick={() => console.log("Click")}>
+				Log Into Spotify
+			</button>
 		</div>
 	);
 }
