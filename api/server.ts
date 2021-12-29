be4454fc617452c3d4ea6d2120400419afc673e5
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-12-28 22:42:42 -0500

    successfully got access token logged to console

--- api/server.ts
+++ api/server.ts
@@ -1,18 +1,67 @@
+require("dotenv").config({ path: ".env" });
+
 import express from "express";
 import cors from "cors";
+import fetch from "cross-fetch";
+
+const port = 8000;
+const clientID = process.env.SPOTIFY_CLIENT_ID;
+const redirect_url = process.env.SPOTIFY_REDIRECT_URL;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+async function requestAccessToken(code: string) {
+	if (typeof code === "string" && typeof redirect_url === "string") {
+		const authString = Buffer.from(`${clientID}:${clientSecret}`).toString(
+			"base64"
+		);
+
+		const auth = `Basic ${authString}`;
+		const body = new URLSearchParams({
+			code: code,
+			redirect_uri: redirect_url,
+			grant_type: "authorization_code",
+		});
+
+		console.log("bod", body);
+
+		const response = await fetch("https://accounts.spotify.com/api/token", {
+			method: "POST",
+			headers: {
+				Authorization: auth,
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			body: body,
+		});
+
+		const json = await response.json();
+		return json;
+	}
+}
 
 const app = express();
 
-const port = 8000;
-
 app.use(cors());
 
-app.get("/", (req, res, next) => {
-	const { query } = req;
-	console.log(query);
+// app.get("/", (req, res, next) => {
+// 	res.send({ response: "hello world" });
+// });
 
-	res.send({ response: "hello world" });
+app.get("/redirect", async (req, res, next) => {
+	console.log("redirect");
+
+	const { code, state, error } = req.query;
+
+	if (!error && typeof code === "string") {
+		const accessToken = await requestAccessToken(code);
+		console.log(accessToken);
+
+		res.send({ code, state });
+	} else {
+		res.send({ error, state });
+	}
 });
+
+app.post("");
 
 app.listen(port, () => {
 	console.log(`Server running on port: ${port}`);
