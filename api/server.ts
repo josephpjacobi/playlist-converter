c2ecaeaafd83922b1092d01d1f5f717bfc2434db
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-12-29 14:19:38 -0500

    added function to build spotify headers object

--- api/server.ts
+++ api/server.ts
@@ -9,13 +9,17 @@ const clientID = process.env.SPOTIFY_CLIENT_ID;
 const redirect_url = process.env.SPOTIFY_REDIRECT_URL;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
+function buildSpotifyHeadersObject() {
+	return {
+		Authorization:
+			"Basic " +
+			Buffer.from(`${clientID}:${clientSecret}`).toString("base64"),
+		"Content-Type": "application/x-www-form-urlencoded",
+	};
+}
+
 async function requestAccessToken(code: string) {
 	if (typeof code === "string" && typeof redirect_url === "string") {
-		const authString = Buffer.from(`${clientID}:${clientSecret}`).toString(
-			"base64"
-		);
-
-		const auth = `Basic ${authString}`;
 		const body = new URLSearchParams({
 			code: code,
 			redirect_uri: redirect_url,
@@ -24,10 +28,7 @@ async function requestAccessToken(code: string) {
 
 		const response = await fetch("https://accounts.spotify.com/api/token", {
 			method: "POST",
-			headers: {
-				Authorization: auth,
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
+			headers: buildSpotifyHeadersObject(),
 			body: body,
 		});
 
@@ -36,13 +37,13 @@ async function requestAccessToken(code: string) {
 	}
 }
 
+async function refreshAccessToken(refreshToken: string) {}
+
 const app = express();
 
 app.use(cors());
 
 app.get("/redirect", async (req, res, next) => {
-	console.log("redirect");
-
 	const { code, state, error } = req.query;
 
 	if (!error && typeof code === "string") {
@@ -55,8 +56,6 @@ app.get("/redirect", async (req, res, next) => {
 		res.send({ error, state });
 	}
 });
-
-app.post("");
 
 app.listen(port, () => {
 	console.log(`Server running on port: ${port}`);
